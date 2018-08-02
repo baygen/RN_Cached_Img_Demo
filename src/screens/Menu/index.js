16e20294c6197323dd1bb422bffb8d7abff9a076
Oleh Bai <oleh.bai@eliftech.com>
2018-08-03 00:11:04 +0300

    added tabs to menu screen

--- src/screens/Menu/index.js
+++ src/screens/Menu/index.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { View, Text, TextInput, Picker } from 'react-native';
 import { connect } from 'react-redux';
 import Styles from '../../common/Styles';
+import TypesFilter from '../../common/TabFilter/TypesFilter';
 
-// @connect(state => ({
-//   filter: state.feed.filterType
-// }), { setFilter, setAndApplyFilter })
+@connect(state => ({
+  filter: state.filter.value
+}))
 export default class Menu extends Component {
   state = {
 
@@ -16,24 +17,26 @@ export default class Menu extends Component {
   }
 
   onFilterChange = (text) => {
-    this.props.setAndApplyFilter(text)
     this.props.navigation.closeDrawer();
   }
 
   render() {
-    // const { navigation: { state: { routeName } } } = this.props;
     return (
       <View style={[Styles.backgroundColorGray, Styles.centeredContainerColumn]}>
         <Text >Select filter to apply </Text>
         <Text >{this.props.filter||' .'}</Text>
-        <Picker
+
+        <TypesFilter
+          onChange={this.onFilterChange}
+        />
+        {/* <Picker
           selectedValue={this.props.filter}
           style={{ height: 50, width: 150}}
           onValueChange={this.onFilterChange}>
           <Picker.Item label="Photo" value="photoPost" />
           <Picker.Item label="Messages" value="messagePost" />
           <Picker.Item label="All" value="" />
-        </Picker>
+        </Picker> */}
       </View>
     );
   }
