82e4dedaa3317a93d95507883cf0b97e4a382694
Oleh Bai <oleh.bai@eliftech.com>
2018-08-03 00:10:14 +0300

    finished tabs component

--- src/common/TabFilter/TypesFilter.js
+++ src/common/TabFilter/TypesFilter.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { View, ToastAndroid } from 'react-native';
 import { connect } from 'react-redux';
-import SegmentedControlTab from 'react-native-segmented-control-tab'
+import SegmentedControlTab from 'react-native-segmented-control-tab';
+import propTypes from 'prop-types';
+
 import Styles from '../Styles';
 import { filters } from '../../AppConstants';
 import { setAndApplyFilter } from './logic';
@@ -12,24 +14,32 @@ import { setAndApplyFilter } from './logic';
     setAndApplyFilter
   })
 export default class TypesFilter extends Component {
+  static props = {
+    onChange: propTypes.func,
+    containerStyle: propTypes.shape({}),
+    tabsProps: propTypes.shape({})
+  }
+
   state = {
     selectedIndex: 0
   }
 
   labels = filters.typeFilter.labels
 
-  _onFilterChange = (index) => {
-    this.props.setAndApplyFilter(filters.typeFilter.values[index], this._show)
-  }
-
-  _show = (l,filter) => {
-    ToastAndroid.show(`Filter : ${filter},,, ${l}`, 800)
-
-  }
-
   componentWillReceiveProps(nProps) {
     const type = nProps.filter.value;
     this._setSelectedIndex(filters.typeFilter.values.findIndex(e => e === type))
+  }
+
+  _onFilterChange = (index) => {
+    const value = filters.typeFilter.values[index];
+    const label = this.labels[index];
+    this.props.setAndApplyFilter(value, this._show);
+    this.props.onChange && this.props.onChange({ value, label, index });
+  }
+
+  _show = (l, filter) => {
+    ToastAndroid.show(`Filter : ${filter},,, ${l}`, 800)
   }
 
   _setSelectedIndex = selectedIndex => this.setState({ selectedIndex })
@@ -38,6 +48,7 @@ export default class TypesFilter extends Component {
     return (
       <View style={[Styles.centeredContainerRow, this.props.style]}>
         <SegmentedControlTab
+          {...this.props.tabsProps}
           values={this.labels}
           selectedIndex={this.state.selectedIndex}
           onTabPress={this._onFilterChange}
