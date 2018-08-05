74f6dd9029de64f3ba250904aa5c55cf94d30579
Oleh Bai <oleh.bai@eliftech.com>
2018-08-05 13:14:13 +0300

    added LargImage component, made some fixes

--- src/common/Post/components/index.js
+++ src/common/Post/components/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
-import FastImage from 'react-native-fast-image'
+import moment from 'moment';
+import FastImage from 'react-native-fast-image';
 
 import Styles from '../../Styles';
-// import {} from '../../../../assets/'
-const avatarPlaceholder = require('../../../../assets/avatar.png');
+const SRC_AVATAR_DEFAULT = require('../../../../assets/avatar.png');
+const SRC_IMAGE_DEFAULT = require('../../../../assets/earth.jpg');
 
-export const AvatarIcon = ({ url, style }) => (
+export const AvatarIcon = ({ url, style = {} }) => (
   <FastImage
     source={{
       uri: url,
       priority: FastImage.priority.normal,
     }}
-    defaultSource={avatarPlaceholder}
+    defaultSource={SRC_AVATAR_DEFAULT}
     style={[{ margin: 5 }, Styles.avatarIcon, style]} />
-)
+);
+
+export const LargeImage = ({ url, style = {} }) => (
+  <FastImage
+    source={{
+      uri: url,
+      priority: FastImage.priority.normal,
+    }}
+    resizeMode={FastImage.resizeMode.cover}
+    defaultSource={SRC_IMAGE_DEFAULT}
+    style={[Styles.postImage, style]} />
+);
 
 export const BottomPart = ({ commentsCount = 0, likesCount = null }) => (
   <View style={{
@@ -22,10 +34,11 @@ export const BottomPart = ({ commentsCount = 0, likesCount = null }) => (
     justifyContent: likesCount ? 'space-between' : 'center',
     height: 30
   }} >
-    <Text > {commentsCount} comments</Text>
     {likesCount && <Text>{likesCount} likes</Text>}
+    <Text > {commentsCount} comments</Text>
+    {likesCount && <Text>{moment().format('M D, HH:mm')}</Text>}
   </View>
-)
+);
 
 export const UserName = ({
   name = 'Unkhown User',
@@ -37,4 +50,4 @@ export const UserName = ({
     }, styles.container || {}]} >
       <Text style={[{ fontSize: 20, fontWeight: '500' }, styles.textStyle || {}]} >{name}</Text>
     </View>
-  )
+  );
