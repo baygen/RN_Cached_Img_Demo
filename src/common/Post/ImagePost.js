1c0d7b15037a18d093df66bb19b7ad1fca5926cf
Oleh Bai <oleh.bai@eliftech.com>
2018-08-05 18:16:04 +0300

    added bottom part

--- src/common/Post/ImagePost.js
+++ src/common/Post/ImagePost.js
@@ -1,10 +1,9 @@
 import React, { PureComponent } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View } from 'react-native';
 import PropTypes from 'prop-types';
 import { BottomPart, UserName, LargeImage } from './components';
-import Styles from '../Styles';
 
-// @connect(state => ({}))
+
 export default class ImagePost extends PureComponent {
   static propTypes = {
     userName: PropTypes.string.isRequired,
@@ -16,36 +15,29 @@ export default class ImagePost extends PureComponent {
   }
 
   render() {
+
     return (
-      // <View style={{ width: '100%', flexDirection: 'column' }}>
       <View style={{
         flex: 1,
         width: '100%',
         flexDirection: 'column',
-        // flexWrap: 'wrap',
         borderBottomColor: 'gray',
         borderBottomWidth: 1,
-        // paddingHorizontal: 10,
       }}>
-        {/* <AvatarIcon url={this.props.imageURL} /> */}
-        {/* <View style={{ flexDirection: 'column', flex: 1 }}> */}
         <UserName name={this.props.userName}
           styles={{
             container: {
-              justifyContent: 'flex-start'
+              justifyContent: 'center'
             }
           }} />
+        <LargeImage
+          url={this.props.imageURL} />
 
-        {/* <Text style={{}} >{this.props.message}</Text> */}
-
-        {/* </View> */}
         <BottomPart
           likesCount={this.props.likesCount}
           commentsCount={this.props.commentsCount}
         />
       </View>
-
-      // </View >
     );
   }
 }
