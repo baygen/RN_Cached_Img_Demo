1bea2f701753ffbbeaf1d4fa24514c38ae749a01
Oleh Bai <oleh.bai@eliftech.com>
2018-08-05 13:16:02 +0300

    added user check while the app loading

--- src/screens/AppLoading/index.js
+++ src/screens/AppLoading/index.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import { ToastAndroid } from 'react-native';
 import { connect } from 'react-redux';
 import LoadingView from '../../common/LoadingScreen';
+import { checkUserAuthorization } from './logic';
 
+@connect(
+  s => ({}),
+  { checkUserAuthorization }
+)
 export default class LoadingScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.props.checkUserAuthorization();
+  }
 
   _goApp = () => {
-    ToastAndroid.show(JSON.stringify(this.props.navigation), 3000)
+    ToastAndroid.show(JSON.stringify(this.props.navigation), 3000);
     this.props.navigation.navigate('Feed');
   }
 
+  // _goAuth = () => {
+  //   this.props.navigation.navigate('Feed');
+  // }
+
   render() {
-    return (<LoadingView onPress={()=>this._goApp()} />);
+    return (<LoadingView onPress={() => this._goApp()} />);
   }
 }
--- src/screens/AppLoading/logic.js
+++ src/screens/AppLoading/logic.js
@@ -1,9 +1,9 @@
 import { NavigationActions } from 'react-navigation';
-import { SCREENS } from '../../navigator/routes';
+import { getUser } from '../../modules/LocalStorage';
+import { ROUTES } from '../../navigator/routes';
 
-export const goTo = (routeName) => async dispatch => {
-  // dispatch()
+export const checkUserAuthorization = () => async dispatch => {
   console.log('LOGIC NAVIGATE goTo');
-
-  dispatch(NavigationActions.navigate({ routeName: routeName}));
+  const nextRoute = (await getUser()) ? ROUTES.APP : ROUTES.AUTH;
+  dispatch(NavigationActions.navigate({ routeName: nextRoute }));
 };
